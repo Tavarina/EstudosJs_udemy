d74c460e08e138752b36552a2e00d558b3fd0e53
Fabiana Oliveira <fabi.ana_deoliveira@outlook.com>
2022-01-07 11:33:23 -0300

    Estudos e desafios

---
+++ JavaScript/inicio/dados_4.js
@@ -0,0 +1,11 @@
+// let a = 3;
+
+// a  = 10; //quando você quer atribuir um novo valor na variavel
+// a = a + 10;
+
+// console.log(a);
+
+
+const a = 3; //uma constante, significa que não posso mais modificar esse valor
+console.log(a);
+
---
+++ JavaScript/inicio/desafio_area.js
@@ -0,0 +1,12 @@
+// PI * raio * raio
+//PI = 3.141592
+
+const raio = 10;
+const PI = 3.14;
+let areaCirc = PI * raio * raio;
+
+console.log("O valor da área é " + areaCirc + "m2.");
+// console.log(Math.PI); // o math dá uma precisão maior.
+
+areaCirc = Math.PI * raio * raio;
+console.log("O valor da área é " + areaCirc + "m2.");
