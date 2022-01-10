07cf4178ab6fefee7f68c80f374836a67c7174b0
Fabiana Oliveira <fabi.ana_deoliveira@outlook.com>
2022-01-10 13:41:29 -0300

    Estudos

---
+++ JavaScript/fundamentos/numerosAlgunsCuidados.js
@@ -0,0 +1,7 @@
+console.log(7 / 0) //Ele apresenta o valor no terminal como infinity
+console.log("10" / 2) // Se ele consegue ler o numero, vai dividir e dar o resultado
+console.log("10.3" / 2) // Ele vai trazer no NaN (Valor incorreto) pq ele não reconhece o 10.3
+console.log("Show!" * 2) //Incorreto
+console.log(0.1 + 0.7) //Ele vai resultar um infinito 0.799999
+// console.log(10.toString()) //Vai dar erro no terminal
+console.log((10.345).toFixed(2)) //Vai trazer a resposta no terminal
