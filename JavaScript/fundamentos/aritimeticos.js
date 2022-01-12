225968a886aaf759e9bb042585fbf56ddfe53a81
Fabiana Oliveira <fabi.ana_deoliveira@outlook.com>
2022-01-12 17:42:12 -0300

    Estudos

---
+++ JavaScript/fundamentos/aritimeticos.js
@@ -0,0 +1,10 @@
+const [a, b, c, d] = [3, 5, 1, 15]
+
+const soma = a + b + c + d
+const subtracao = d - b
+const multiplicacao = a * b
+const divisao = d / a
+const modulo = a % 2
+
+console.log(soma, subtracao, multiplicacao, -divisao, modulo) //se vc colocar o - na frente do operador, por exemplo: -divisao ele irá trazer o resultado em negativo
+
