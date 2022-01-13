12e3777b4dd9807b60ff9143a833843e862ea12d
Fabiana Oliveira <fabi.ana_deoliveira@outlook.com>
2022-01-13 15:15:50 -0300

    Estudos

---
+++ JavaScript/fundamentos/unarios.js
@@ -0,0 +1,15 @@
+let num1 = 1
+let num2 = 2
+
+
+num1++
+ //ele acrescenta uma unidade ao valor da variavel
+ console.log(num1)
+ --num1
+ console.log(num1)
+
+ console.log(++num1 === num2--)  // vai dar verdadeiro
+ console.log(num1 === num2)  //vai dar false
+
+ //deixar o código mais simples possivel
+
