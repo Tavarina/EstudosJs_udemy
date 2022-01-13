62d3d58b725de97d078f5df7f01484ae93e13616
Fabiana Oliveira <fabi.ana_deoliveira@outlook.com>
2022-01-13 15:28:16 -0300

    Estudos operador ternario

--- JavaScript/fundamentos/ternario.js
+++ JavaScript/fundamentos/ternario.js
@@ -1,6 +1,13 @@
 const resultado = nota => nota >= 7 ? 'Aprovado' : 'Reprovado'
 
-onsole.log(resultado(7.1))
-onsole.log(resultado(6.7))
+console.log(resultado(7.1))
+console.log(resultado(6.7))
 
 // const status = nota >= 7 ? 'Aprovado' : 'Reprovado'
+
+//outra opcao q funcionaria da mesma forma
+const resultado = nota => {
+    return nota >= 7 ? 'Aprovado' : 'Reprovado'
+} 
+console.log(resultado(7.1))
+console.log(resultado(6.7))
