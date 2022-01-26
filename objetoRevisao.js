df3dc026595e1fb64540198c682d4c5146cc0cc7
Fabiana Oliveira <fabi.ana_deoliveira@outlook.com>
2022-01-26 15:46:31 -0300

    Estudos OO

---
+++ objetoRevisao.js
@@ -0,0 +1,31 @@
+// objeto é uma coleção dinâmica de pares chave/valor
+
+const produto = new Object
+produto.nome = 'Cadeira'
+produto['marca do produto'] = 'Generica'
+produto.preco = 220
+
+console.log(produto)
+delete produto.preco
+delete produto['marca do produto']
+console.log(produto)
+
+const carro = {
+    modelo: 'A4',
+    valor: 8900,
+    proprietario: {
+        nome: ' Raul',
+        idade: 56,
+        endereco: {
+            longadouro:'Rua Abc',
+            numero: 123
+        }
+    },
+    condutores: [{
+    nome: ' Junior',
+    idade: 19
+    }, {
+        nome: ' Liu',
+        idade: 45
+    }]
+}
