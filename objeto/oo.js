ce968b8930742f36174ca334d8429d2000e304d7
Fabiana Oliveira <fabi.ana_deoliveira@outlook.com>
2022-01-26 13:12:18 -0300

    Estudos

---
+++ objeto/oo.js
@@ -0,0 +1,18 @@
+// Código não executável!!!
+
+//Procedural
+processamento(valor1, valor2, valor3)
+
+// OO (Orientação ao objeto)
+objeto = {
+    valor1,
+    valor2,
+    valor3,
+    processamento() {
+        //...
+    }
+}
+
+objeto.processamento()
+
+//o objeto funciona como uma capsula que agrupa comportamentos e atributos
