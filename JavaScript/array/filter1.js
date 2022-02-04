12b8b2ceee69dfc17b77183c345cc303cef98dd2
Fabiana Oliveira <fabi.ana_deoliveira@outlook.com>
2022-02-04 15:01:44 -0300

    Estudos Array/reduce

--- JavaScript/array/filter1.js
+++ JavaScript/array/filter1.js
@@ -2,7 +2,7 @@ const produtos = [
     { nome: 'Notebook', preco: 2499, fragil: true },
     { nome: 'iPad Pro', preco: 4199, fragil: true },
     { nome: 'Copo de Vidro', preco: 12.49, fragil: true },
-    { nome: 'Copo de Plástico', preco: 18.99, fragil: false },
+    { nome: 'Copo de Plástico', preco: 18.99, fragil: false }
 ]
 console.log(produtos.filter(function(p){
     return false 
--- JavaScript/array/filter2.js
+++ JavaScript/array/filter2.js
@@ -12,7 +12,7 @@ const produtos = [
     { nome: 'Notebook', preco: 2499, fragil: true },
     { nome: 'iPad Pro', preco: 4199, fragil: true },
     { nome: 'Copo de Vidro', preco: 12.49, fragil: true },
-    { nome: 'Copo de Plástico', preco: 18.99, fragil: false },
+    { nome: 'Copo de Plástico', preco: 18.99, fragil: false }
 ]
 
 const caro = produto => produto.preco >= 500
