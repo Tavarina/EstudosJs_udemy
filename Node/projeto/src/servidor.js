8ec8257e1f6061b4fc93f65787cfc156c862c47c
tavarina <fabi.ana_deoliveira@outlook.com>
2022-02-24 11:48:50 -0300

    Projeto Api - estudos

--- Node/projeto/src/servidor.js
+++ Node/projeto/src/servidor.js
@@ -4,8 +4,16 @@ const express = require('express')
 const app = express()
 
 app.get('/produto', (req, res, next) =>{
+    console.log('Middleware 1...')
+    next()
+})
+
+app.get('/produto', (req, res, next) =>{
     res.send({ nome: 'Notebook', preco: 123.45 }) //Converter para JSON
 })
+// app.use('/produto', (req, res, next) =>{
+//     res.send({ nome: 'Notebook', preco: 123.45 }) //Converter para JSON
+// })  // assim vc pode chamar por todas as requisições - use o postman para testar
 
 app.listen(porta, () => {
     console.log(`Servidor executando na porta ${porta}.`)
